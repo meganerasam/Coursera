b0c62dc18b28fd674d59668339065c4d5ea60e11
Megane Rasamimanana <meganerasam@gmail.com>
2019-01-25 13:15:31 +0400

    Up to Assignment 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -49,6 +50,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   providers: [
     DishService,
     PromotionService,
+    LeaderService,
   ],
   bootstrap: [AppComponent]
 })
